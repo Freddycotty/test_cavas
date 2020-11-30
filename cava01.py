dede12261c20ec9dfd5ad600fcecef916803fe17
freddycotty <freddycarrillo1912@gmail.com>
2020-11-30 10:10:15 -0400

    Proyecto de cavas optimizado optimizado

--- cava01.py
+++ cava01.py
@@ -1,177 +1,91 @@
+import os, glob, time, hug, pymysql,json
 
-from datetime import datetime
-from random import randrange
-import os
-import glob
-import time
-import hug
-import mysql.connector as mysql
-import time
-from datetime import date
-from datetime import datetime
-from threading import Thread
-import datetime
-import pytds
-import requests
-import json
+from datetime import datetime, date
+from clases import DataBase, Alerts
+
+
+
 # Leer Json
 with open('config.JSON') as config_file:
         data = json.load(config_file)
-        pass
 
 
 
-SQLserver = data['SQLserver']
 Temperatura = data['Temperatura']
 Mensaje = data['Mensaje']
-Telegran = Mensaje['Telegran']  
+Telegram = Mensaje['Telegram']  
+Activar = Mensaje['Generar_Alerta'] 
+msj = Telegram['Link']
 
-    
-
-# Mysql
-
-"""
-db = mysql.connect(
-    host="us8.mysql.gonavi.dev",
-    user="root",
-    passwd="Server$$123",
-    database = "temperatura"
-)
-cursor = db.cursor()
-"""
-# insertar Datos Mysql
-"""
-def Insertar(Temp,Fecha):
-    query = "INSERT INTO Cava1 (Cava1, Temp, Fecha) VALUES (%s, %s, %s)"
-    values = ("Temperatura", Temp, Fecha)
-    cursor.execute(query, values)
-    db.commit()
-    
-    print(cursor.rowcount, "record inserted")
-"""      
+# Objects
+DataBase = DataBase()
+Alerts = Alerts()
+  
 #--- HARWARE --- 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[Mensaje['Cava']]
+
+"""device_folder = glob.glob(base_dir + '28*')[Mensaje['Cava']]
 device_file = device_folder + '/w1_slave'
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
-    return lines
-def read_temp():
-    pass
-       
-       
+    return lines"""
+
+
 # Insertar Datos En el grafico   
-    while True:
-        def CV():
-            lines = read_temp_raw()
-            while lines[0].strip()[-3:] != 'YES':
-                print("1")
-                lines = read_temp_raw()
-            equals_pos = lines[1].find('t=')
-            if equals_pos != -1:
-                temp_string = lines[1][equals_pos+2:]
-                temp_c = int(temp_string) / 1000
-                Now2 = int(temp_c)
-                return Now2
+def read_temp():
+  pass
 
+  while True:
+      """def CV():
+          lines = read_temp_raw()
+          while lines[0].strip()[-3:] != 'YES':
+              print("1")
+              lines = read_temp_raw()
+          equals_pos = lines[1].find('t=')
+          if equals_pos != -1:
+              temp_string = lines[1][equals_pos+2:]
+              temp_c = int(temp_string) / 1000
+              Now2 = int(temp_c)
+              return Now2"""
+
+          
+      
+      time.sleep(Temperatura['Sengundos']) 
+      Global = datetime.now() #Fecha & Hora Global
+      TimeStamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #Fecha & Hora en TimeStamp
+      
+      try:
+          now = int(-10) #TEMPERATURA
+          Max = int(-2) #TEMPERATURA MAXIMA
+          Min = int(20) #TEMPERATURA MINIMA
+          DataBase.insert_temp(1, now,TimeStamp) #INSERT
+          query = DataBase.select_temp(1) #SELECT 
+          
+          #DEFINIMOS LAS VARIABLES DE LA CLASE 
+          Alerts.now = now
+          Alerts.simb = query[2]
+          Alerts.cava = query[1]
+          Alerts.zona = query[0]
+          Alerts.msj = msj
+          
+          
+
+          if Activar == "True":          
+            if now <= Max or now >= Min:               
+              Alerts.messange_error(Alerts.now, Alerts.simb, Alerts.cava, Alerts.zona, Alerts.msj)   #MENSAJE ERROR  
+            else:
+              Alerts.messange(Alerts.now, Alerts.simb, Alerts.cava, Alerts.zona, Alerts.msj)   #MENSAJE HABITUAL   
             
-        
-        time.sleep(Temperatura['Sengundos'])
-        
-
-        Now1 = int(CV())
-        localtime = datetime.datetime.now()
-        Global = str (datetime.datetime.now())
-        #-- borrar cache --#
-        
-        try:
-            """conn = pytds.connect(SQLserver['direccion_IP_servidor'], SQLserver['nombre_de_Base_de_dato'], SQLserver['nombre_de_usuario'], SQLserver['password'])
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM temperaturas')
-
-            def inssert(ID, TEMP, FECHA):
-                query = ("INSERT INTO temperaturas (zona_id, valor, created_at, updated_at) VALUES (%s,%s,%s,%s)")
-                values = (ID, TEMP, FECHA, FECHA)
-                cursor.execute(query,values)
-                conn.commit()
-                pass
-            for Temp in cursor.execute('SELECT max FROM  zonas WHERE id=2'):
-                """
-            Max = int(-2)
-            print("Maximo ",Max)
-            """for Temp in cursor.execute('SELECT min FROM  zonas WHERE id=2'):
-                """
-            Min = int(20)
-            print("Minimo ",Min)   
-            
-            print("Temperatura Registrada: " , Now1 ,"°C " ,localtime)
-            #inssert(Mensaje['Zona'], Now1, localtime)
-             
-        
-        
-            Temperatura12 = Mensaje['Generar_Alerta']
-            Temperatura1 = Now1   
-            if Temperatura12 == "True":
-                Monitor = str(Mensaje['Alerta']+str(Temperatura1) + "°C " + " Cava: " + str(Mensaje['Cava']))
-                msj = Telegran['Link']
-                respuesta = (msj+Monitor)
-                print(Monitor)
-                requests.get(respuesta)
-                pass    
-        # Mensaje mayor                  
-            activar = Mensaje['Activar_alerta_o_desactivar']   
-            if activar == "True":
-                Temperatura1 = Now1
-                #print("Temperatura en:",Temperatura1)
-                if Temperatura1 > Max:
-                    Monitor = str(Mensaje['Mensaje_Alerta'] + str(Temperatura1) + "°C " + Mensaje['Error'] + " Cava: " + str(Mensaje['Cava']))
-                    msj = Telegran['Link']
-                    respuesta = (msj+Monitor)
-                    print(Monitor)
-                    requests.get(respuesta)
-                    pass
-        # Mensaje Igual Mayor        
-            activar = Mensaje['Activar_alerta_o_desactivar']   
-            if activar == "True":
-                Temperatura1 = Now1
-                if Temperatura1 == Max:
-                    Monitor = str(Mensaje['Mensaje_Alerta'] + str(Temperatura1) + "°C " + Mensaje['Error'] + " Cava: " + str(Mensaje['Cava']))
-                    msj = Telegran['Link']
-                    respuesta = (msj+Monitor)
-                    print(Monitor)
-                    requests.get(respuesta)
-                    pass          
-        # Mensaje Igual menor
-            activar = Mensaje['Activar_alerta_o_desactivar']   
-            if activar == "True":
-                Temperatura1 = Now1
-                if Temperatura1 == Min:
-                    Monitor = str(Mensaje['Mensaje_Alerta'] + str(Temperatura1) + "°C " + Mensaje['Error'] + " Cava: " + str(Mensaje['Cava']))
-                    msj = Telegran['Link']
-                    respuesta = (msj+Monitor)
-                    print(Monitor)
-                    requests.get(respuesta)
-                    pass
-        # Mensaje menor
-            activar = Mensaje['Activar_alerta_o_desactivar']   
-            if activar == "True":
-                Temperatura1 = Now1
-                if Temperatura1 < Min: 
-                    Monitor = str(Mensaje['Mensaje_Alerta'] + str(Temperatura1) + "°C " + Mensaje['Error'] + " Cava: " + str(Mensaje['Cava']))
-                    msj = Telegran['Link']
-                    respuesta = (msj+Monitor)
-                    print(Monitor)
-                    requests.get(respuesta)
-                    pass   
                 
-        except IOError:
-                print("Error de entrada o salida")
-        except:
-            print("Error verifique si tienes acceso a internet")
+      except IOError:
+              print("Error de entrada o salida")
+      except:
+          print("Error verifique si tienes acceso a internet")
 
 if __name__ == "__main__":
     while True:
---
+++ clases.py
@@ -0,0 +1,72 @@
+import pymysql, requests 
+
+class DataBase:
+
+
+  #CONEXION
+  def __init__(self):
+    self.connection = pymysql.connect(
+      host="localhost",
+      user="root",
+      passwd="",
+      database = ""
+    )
+
+    self.cursor = self.connection.cursor()
+
+    print("conexion exitosa")
+  
+  #SELECT TEMPERATURA
+  def select_temp(self, id):
+    sql = "SELECT tz.nombre, tc.nombre, tg.simbolo FROM temperatura.zonas tz INNER JOIN temperatura.cavas tc ON tz.cava_id = tc.id INNER JOIN temperatura.grados tg ON tc.grado_id = tg.id WHERE tz.id = {}".format(id)
+
+    try:
+      self.cursor.execute(sql) #EJECUCION
+      user = self.cursor.fetchone() #OBTIENE UN SOLO REGISTRO
+      nombre_zona = user[0]
+      nombre_cava = user[1]
+      simbolo_temperatura = user[2]
+      return nombre_zona, nombre_cava, simbolo_temperatura
+      
+    except Exception as e:
+      print('Ha ocurrido un error')
+
+  #INSERT TEMPERATURA
+  def insert_temp(self, zna, grd, fch):
+    sql = "INSERT INTO temperatura.temperaturas(zona_id, grado, fecha) VALUES ({},{}, '{}')".format(zna,grd,fch)  
+
+    try:
+      self.cursor.execute(sql) #EJECUCION
+      self.connection.commit() #ACTUALIZACION
+    
+    except Exception as e:
+      print('Ha ocurrido un error')
+
+  #CERRAR CONEXION
+  def close(self):
+    self.connection.close()
+
+class Alerts:
+  now = ''
+  simb = ''
+  cava = ''
+  zona = ''
+  msj = '' 
+
+  #MENSAJE HABITUAL
+  def messange(self, now, simb, cava, zona, msj):
+    Monitor = str("Temperatura en: " + str(self.now) + self.simb + " " + self.cava + ", Zona " + self.zona)
+    respuesta = (msj+Monitor)
+    print(Monitor)
+    return requests.get(respuesta)
+
+  #MENSAJE ERROR
+  def messange_error(self, now, simb, cava, zona, msj):
+    Monitor = str("Temperatura en: " + str(self.now) + self.simb + " " + self.cava + ", Zona " + self.zona + ": ERROR")
+    respuesta = (msj+Monitor)
+    print(Monitor)
+    return requests.get(respuesta)
+
+
+
+
